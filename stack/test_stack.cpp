96d4464357a9c674808bbdb235159fe4d6ef4acb
tviksikk <tviksikk@gmail.com>
2017-06-01 16:12:57 +0300

    доп.задание

--- stack/test_stack.cpp
+++ stack/test_stack.cpp
@@ -1,23 +1,44 @@
 #include "stack.h"
 #include <iostream>
+#include <direct.h>
+#include <string>
+#include <sstream>
 
 using namespace std;
+
+Tstack<string>& split(const string &s, char delim, Tstack<string> &elems) {
+	stringstream ss(s);
+	string item;
+	while (getline(ss, item, delim)) {
+		elems.add(item);
+	}
+	return elems;
+}
+
+
+Tstack<string>& split(const string &s, char delim) {
+	Tstack<string> elems(15);
+	return split(s, delim, elems);
+}
+
 
 int main()
 {
 	try
 	{
-    	Tstack <int> temp(10);
+		setlocale(NULL, "RUSSIAN");
+    	Tstack <string> temp;
+
+		char current_work_dir[FILENAME_MAX];
+		_getcwd(current_work_dir, sizeof(current_work_dir));
 	
-		temp.add(123);
-		temp.add(155);
-		temp.add(121);
-		temp.add(0);
-		cout << temp.get() << " ";
-		cout << temp.get() << " ";
-		cout << temp.get() << " ";
-		cout << temp.get();
-		temp.resize(2);
+		temp = split(current_work_dir, '\\');
+
+		for (;;)
+			cout << temp.get() << endl;
+
+
+
 	}
 	catch (int e)
 	{}
